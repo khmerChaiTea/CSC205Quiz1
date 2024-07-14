d621edb1c8933680840d90ea943913a6796df1f3
pchai <prempreychhay@gmail.com>
2024-07-14 08:34:57 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205Quiz1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Get the full path of the executable file
+            string executablePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            // Print the full path to the console
+            Console.WriteLine("Executable Path: " + executablePath);
+
+            // Optionally, you can pause the console so the user can see the output
+            Console.ReadLine();
+
+            Console.WriteLine();
+        }
+    }
+}
